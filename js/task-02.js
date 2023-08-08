546b855fc9bf3e9bbab94014c688d1a5f28f028d
iVladyuser <korol.v09@gmail.com>
2023-08-09 00:14:28 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const ul = document.querySelector(`ul`);
+
+let ingredientsArray = [...ingredients];
+ingredientsArray.forEach((ingred) => {
+	const li = document.createElement(`li`);
+	li.textContent = ingred;
+	li.classList.add("item");
+	ul.append(li);
+});
