7451e92c1645de80e638f9326dde5b813916a3a4
iVladyuser <korol.v09@gmail.com>
2023-08-11 23:48:44 +0200

    task-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
 const input = document.querySelector("#font-size-control");
 const span = document.querySelector("#text");
 
-input.addEventListener('input', (event) => {
-    span.style.fontSize = `${event.currentTarget.value}px`;
-    console.log(event.currentTarget.value);
+input.addEventListener("input", (event) => {
+	span.style.fontSize = `${event.currentTarget.value}px`;
 });
-
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector(".login-form");
+const data = {
+	email: "",
+	password: "",
+};
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+
+	const { email, password } = event.currentTarget.elements;
+
+	if (email.value.trim() === "" || password.value.trim() === "") {
+		alert("Будь ласка, заповніть поля форми!");
+	} else {
+		data.email = email.value;
+		(data.password = password.value), console.log(data);
+		event.currentTarget.reset();
+	}
+
+	// 	for backEnd
+
+	// 	const formData = new FormData(event.currentTarget);
+
+	// 	formData.forEach((value, name) => {
+	// 		console.log("onFormSubmit -> name", name);
+	// 		console.log("onFormSubmit -> value", value);
+	// 	});
+}
